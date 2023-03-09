642a1e8e979a4b8c10402fc624fe7e0cd0684074
joanita-51 <nakityojoanita51@gmail.com>
2023-03-09 15:38:22 +0300

    using get to find users

--- node-express-api/routes/users.js
+++ node-express-api/routes/users.js
@@ -2,9 +2,23 @@ import express from "express";
 
 const router = express.Router();
 
+const users =[
+    {
+        "firstName":"John",
+        "lastName":"Doe",
+        "age":20
+    },
+    {
+        "firstName":"Jane",
+        "lastName":"Doe",
+        "age":23
+    }
+]
+
 //all routes in here are starting with /users
 router.get('/users',(req, res)=>{
-    res.send("Hello")
+    console.log(users)
+    res.send(users)
 });
 
 export default router;
