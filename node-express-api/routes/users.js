32ad2cb8a80e1b58c2628079908f90d08a9691df
joanita-51 <nakityojoanita51@gmail.com>
2023-03-09 19:51:11 +0300

    returning a user based on their id

--- node-express-api/routes/users.js
+++ node-express-api/routes/users.js
@@ -5,15 +5,23 @@ const router = express.Router();
 
 const users =[
     {
-        "firstName":"John",
-        "lastName":"Doe",
-        "age":20
-    },
-    {
-        "firstName":"Jane",
-        "lastName":"Doe",
-        "age":23
-    }
+        firstName: 'Joan',
+        lastName: 'Doe',
+        age: 2,
+        id: '618ee19d-b901-48f7-9db0-a089ea28b0c0'
+      },
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 28,
+        id: '18e07690-1c53-4006-ab34-e7802c9b5f97'
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        age: 25,
+        id: 'b4855144-bdfa-4d07-b3a6-673722ffbc6a'
+      }
 ]
 
 //all routes in here are starting with /users
@@ -24,10 +32,14 @@ router.get('/users',(req, res)=>{
 
 router.post('/',(req, res)=>{
     const user = req.body;
-    const userId = uuidv4();
-    const userWithId = {...user, id:userId}
-    users.push(userWithId)
+    users.push({...user, id:uuidv4()})
     res.send(`user with the name ${user.firstName} added to the database`);
+})
+
+router.get('/users/:id', (req, res)=>{
+    const {id} =req.params
+    const foundUser = users.find((user)=>user.id === id)
+    res.send(foundUser)
 })
 
 export default router;
