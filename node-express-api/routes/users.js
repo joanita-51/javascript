c5c42bf92fbe26adcb9281d803535cb844ac7a3a
joanita-51 <nakityojoanita51@gmail.com>
2023-03-09 18:09:01 +0300

    adding a user with a unique id to the database

--- node-express-api/routes/users.js
+++ node-express-api/routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
@@ -23,7 +24,9 @@ router.get('/users',(req, res)=>{
 
 router.post('/',(req, res)=>{
     const user = req.body;
-    users.push(user)
+    const userId = uuidv4();
+    const userWithId = {...user, id:userId}
+    users.push(userWithId)
     res.send(`user with the name ${user.firstName} added to the database`);
 })
 
