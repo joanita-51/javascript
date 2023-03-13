e19cf2a1e4b6efc54fabedce41ea6e932061115a
joanita-51 <nakityojoanita51@gmail.com>
2023-03-13 15:12:02 +0300

    create and read endpoints using firestore

--- firebase-node/index.js
+++ firebase-node/index.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT||8080;
 
 firebase.initializeApp({
     credential: firebase.credential.cert(credentials)
-})
+});
+
+const db = firebase.firestore();
 
 app.use(bodyParser.json());
 
@@ -21,7 +23,6 @@ app.post('/signup', async( req, res)=>{
     const userResponse = await firebase.auth().createUser({
         email:req.body.email,
         password: req.body.password,
-        userRole:req.body.role
     })
     res.send(userResponse)
 })
@@ -32,7 +33,49 @@ app.post('/login',async(req,res)=>{
     res.send(userLogin)
 })
 
+app.post('/create',async(req,res)=>{
+    try {
+        const userJson ={
+            email:req.body.email,
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            userRole:req.body.userRole
+        };
+        console.log(userJson)
+        const response = await db.collection("users").add(userJson);
+        res.send(response)
+    } catch (error) {
+        res.send(error)
+    }
+})
 
+app.get("/read/all",async(req,res)=>{
+    try {
+        const usersRef = db.collection("users");
+        const response = await usersRef.get ();
+        let responseArr = [];
+        response.forEach((doc)=>{
+            responseArr.push(doc.data());
+        })
+        res.send(responseArr);
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.get('/read/:id', async(req,res)=>{
+    try {
+        const userRef = db.collection("users").doc(req.params.id);
+        const response = await userRef.get()
+        console.log(response)
+        res.send(response.data());
+    } catch (error) {
+        res.send (error)
+    }
+})
+app.post('/update', async(req,res)=>{
+    
+})
 app.listen(PORT,()=>{
     console.log(`The server is running on port ${PORT}`)
 })
